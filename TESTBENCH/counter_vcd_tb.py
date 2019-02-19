77e02715625b8ae9da5b601ada3d24688b162c92
AKel <AaronKelly@email.com>
2019-02-19 21:54:48 +0000

    Adding dump to VCD testbench
    Adding dump at each time step

--- TESTBENCH/counter_vcd_tb.py
+++ TESTBENCH/counter_vcd_tb.py
@@ -11,11 +11,16 @@ VCD.open("counter_dump.vcd")
 
 
 def clk_toggle(n=1):
+    global main_time
     for _ in range(n):
         top.clk = 0
         top.eval()
+        main_time += 1
+        VCD.dump(main_time)
         top.clk = 1
         top.eval()
+        main_time += 1
+        VCD.dump(main_time)
 
 
 top.enable = 1
